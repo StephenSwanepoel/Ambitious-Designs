9941d2dc8aaacfd6532c4a6def14ab4e194b1427
Stephen <u11032091@tuks.co.za>
2016-09-06 00:05:45 +0200

    updated main file

---
+++ Smart%20Image%20Identifier/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+
+import org.opencv.core.Core;
+
+import com.codeferm.opencv.*;
+import com.codeferm.opencv.DefualtImpl.*;;
+
+public class Main {
+
+	public static void main(final String... args) throws IOException {
+	
+		PeopleDetect p = new PeopleDetectImplementation();
+		Database database = new Database();
+		
+		File folder = new File("./resources/database");
+		File[] listOfFiles = folder.listFiles();
+		
+		for(int i=0; i< listOfFiles.length; i++)
+		{
+			System.out.println(listOfFiles[i].getPath());
+			if(listOfFiles[i].getPath().contains(".jpg"))
+				p.runTests(listOfFiles[i].getPath());
+		}
+	}
+    
+}
