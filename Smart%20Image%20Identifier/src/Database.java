1978e87c8d854a07dda62234d172c275cb11139c
Dian Veldsman <u12081095@users.noreply.github.com>
2016-09-07 18:31:08 +0200

    Update Database.java

--- Smart%20Image%20Identifier/src/Database.java
+++ Smart%20Image%20Identifier/src/Database.java
@@ -1,65 +1,105 @@
+package com.codeferm.opencv;
+
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.Base64;
 import javax.imageio.ImageIO;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
-//import com.mongodb.Block;
-import com.mongodb.client.FindIterable;
-
-//import static com.mongodb.client.model.Filters.*;
-//import static com.mongodb.client.model.Sorts.ascending;
-import static java.util.Arrays.asList;
 
 import java.awt.image.BufferedImage;
 
 public class Database {
 	
-	public static void main(String[] args) {
-		MongoClient mongoClient = new MongoClient();
-		MongoDatabase db = mongoClient.getDatabase("test");
-		FindIterable<Document> iterable = db.getCollection("restaurants").find();
-		System.out.println("ASD");
-	}	
+	private static MongoClient mongoClient;
+	private static DB db;
 	
-	public byte[] getDecoded(byte[] encodedBytes){
+	 /**
+     * Method which connects to the database and pulls records 
+     */
+	public static Boolean main(String[] args) {
+		try {
+        //=================== CONNECT TO DB ========================
+		mongoClient = new MongoClient("localhost", 27017);
+		db =mongoClient.getDB("COS301");
+		
+		System.out.println("Connected to database successfully.");
+		readDatabase();
+		
+		return true;
+		
+		} catch (Exception e) {
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+        }
+		
+		return false;
+	}
+	
+	 /**
+     * Method which decodes a byte array
+     * @param encodedBytes is a byte array which contains the data required to produce an image
+     * @return returns decoded base64 string
+     */
+	public static byte[] getDecoded(byte[] encodedBytes){
 		return Base64.getDecoder().decode(encodedBytes);
 	}
 	
-	public byte[] getEncoded(byte[] decodedBytes){
+	 /**
+     * Method which encodes a byte array
+     * @param decodedBytes is a byte array which contains the data required to produce an image
+     * @return returns encoded base64 string
+     */
+	public static byte[] getEncoded(byte[] decodedBytes){
 		return Base64.getEncoder().encode(decodedBytes);
 	}
 		
-	public void readDatabase(MongoDatabase db)
+	 /**
+     * Method which reads .bson objects from the database, storing the necessary details 
+     * and writing the image to the folder for processing
+     */
+	public static void readDatabase() throws IOException
     {
-        /*try
+        BasicDBObject query = new BasicDBObject();
+    	BasicDBObject field = new BasicDBObject();
+        	field.put("attachments", 0);
+    	BasicDBObject attachments = new BasicDBObject();
+    	attachments.put("attachments.fields",java.util.regex.Pattern.compile("")); 
+        
+        DBCursor items = db.getCollection("Willburg").find(query,field);
+        DBCursor items2 = db.getCollection("Willburg").find(query,attachments);    
+                
+        for(int i=0; i<50; i++)
         {
-            byte[] byteArray = new byte[102400];
-            FileInputStream fis = new FileInputStream(file);
-            byte[] base64String;
-            int bytesRead = 0;
-            while ((bytesRead = fis.read(byteArray)) != -1)
-            {
-            	byte[] encodedBytes = Base64.getEncoder().encode(byteArray);
-                base64String = Base64.getDecoder().decode(encodedBytes);
-                buildImage(file, base64String, directory);
-            }
-            fis.close();
+        	BasicDBObject obj = (BasicDBObject) items.next();
+        	BasicDBObject obj2 = (BasicDBObject) items2.next();
+            String cameraID = obj.get("camera").toString();
+            String date = obj.get("date").toString();
+            String from = obj.get("from").toString();
+            String ID = obj.get("_id").toString();
+            String encoding = obj2.get("attachments").toString().substring(18, obj2.get("attachments").toString().length()-4);
+            
+            BufferedImage img = getImage(encoding.getBytes());
+            //Write the image to a file
+            File outputfile = new File("./resources/database/"+ ID +".jpg");
+            ImageIO.write(img, "jpg", outputfile);
         }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }*/
     }	
 	
-	public BufferedImage getImage(File file, byte[] base64String) throws IOException{
-		
+	 /**
+     * Method which generates an image from a base64 string
+     * @param base64String is a byte array which contains the data required to produce an image
+     * @return returns buffered image
+     */
+	public static BufferedImage getImage(byte[] base64String) throws IOException{		
 		try{
 			BufferedImage image = null;
-			ByteArrayInputStream bis = new ByteArrayInputStream(base64String);
+			byte[] imageByte = getDecoded(base64String);			
+			
+			ByteArrayInputStream bis = new ByteArrayInputStream(imageByte);
 			image = ImageIO.read(bis);
 			bis.close();
 			
