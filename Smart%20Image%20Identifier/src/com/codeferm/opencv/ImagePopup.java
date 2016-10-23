7053bae4cb052bc1af4741a7167b8b003ad5d226
Dian Veldsman <dianveldsman1993@gmail.com>
2016-10-23 22:24:29 +0200

    Changes to Documents and Repository cleanup

--- Smart%20Image%20Identifier/src/com/codeferm/opencv/ImagePopup.java
+++
@@ -1,9 +0,0 @@
-package com.codeferm.opencv;
-
-import java.awt.image.BufferedImage;
-
-public interface ImagePopup {
-	public void popup(BufferedImage bufferedimage); 
-	public void popup(String Url); 
-	public void ClosePopup();
-}
